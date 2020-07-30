30a696e935e36fa98833799ba15b6c7702654141
Vitaly <vitaly.zubentsov@ya.ru>
2020-07-30 21:37:58 +0300

    Реализовал разбивку логов на массив строк с последующей фильтрацией и выводом в консоль только логов коммутации.

--- src/main/java/TetraLog.java
+++ src/main/java/TetraLog.java
@@ -1,18 +1,17 @@
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class TetraLog {
 
     public static void main(String[] args) {
 
-        // FileReader fileWithLogs = new FileReader(new File("/main/resources/commutation.txt"));
         String stringWithLogs = "";
         try (Scanner scannerForFileWithLogs = new Scanner(new File("src/main/resources/commutation.txt"))) {
-            StringBuilder stringBuilder = new StringBuilder();
+               StringBuilder stringBuilder = new StringBuilder();
 
             while (scannerForFileWithLogs.hasNextLine()) {
                 stringBuilder.append(scannerForFileWithLogs.nextLine()).append("\n");
@@ -22,8 +21,14 @@ public class TetraLog {
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-        System.out.println(stringWithLogs);
 
-        //TODO разбить логи по регулярке  ^(0\\d{1}.\\d{7}, )
+        //Разбиваем строку в соотвествии с регулярным выражением для поиска идентификатора сообщения
+        //Сам идентификатор отбрасывается, так как для приложения не будет использваться
+        //при разбиении образуется совпадение нулевой длины в начале строки
+        String[] arrayOfCommutationMessages = stringWithLogs.split("0\\d\\.\\d{5},", -1);
+
+        //Используем фильтр и выводим сообщения коммутации
+        Arrays.stream(arrayOfCommutationMessages).parallel().filter(Pattern.compile("E1_CONNECTION-").asPredicate()).forEach(System.out::println);
+
     }
 }
